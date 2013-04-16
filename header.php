e3a201b1a9c94c3af152595d44eb3141fa34fbda
dhodge1 <dhodge1@gmail.com>
2013-04-16 14:41:44 -0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html
+PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US">
+<head>
+<!--Author: David Hodge
+  C#: c2530a06
+    Lab# 7
+    Dated Created: 02/25/2013
+    Date Modified: 04/02/2013
+    Course: CSIT 2530
+-->
+<meta name="Keywords" content="list of appropriate keywords"/>
+<meta name="Description" content="relevant description"/>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<!-- <link rel="stylesheet"  type="text/css" href="myform.css" title="stylesheet"/> -->
+<title>Internship Application</title>
+<img src="http://i.imgur.com/bYceqSA.gif" align="middle">
+</head>
